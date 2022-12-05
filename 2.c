4fb6bb32f4fb2d3e0f78f9822f83acf8a4f32a00
Laurynas-Zukauskas <118725516+Laurynas-Zukauskas@users.noreply.github.com>
2022-12-05 19:11:52 +0200

    Add files via upload

---
+++ 2.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+int main(){
+    int a, b, c;
+    int Array[1000];
+    srand(time(NULL));
+    printf("Si programa apibrezia sveikuju skaiciu masyva, kurio nariai yra atsitiktiniai skaiciai, priklausantys vartotojo apibreztam intervalui.\n");
+    printf("Iveskite intervalo ribas:\n");
+    scanf("%d%d", &a, &b);
+    if(a >= b){
+        printf("Antra masyvo riba turi buti didesne uz pirma.");
+        return 0;
+    }
+    printf("Iveskite masyvo talpa:\n");
+    scanf("%d", &c);
+    if(c < 1){
+        printf("masyve negali buti neigiamas skaicius nariu.");
+        return 0;
+    }
+    for(int i = 0; i < c; ++i){
+        Array[i] = rand() % (b - a + 1) + a;
+        printf("%d ", Array[i]);
+    }
+    return 0;
+}
